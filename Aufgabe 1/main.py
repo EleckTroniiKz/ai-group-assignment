938d9f8128a903307676c4a88bbd2c8dcc0a3a40
Can Cetin <73250162+EleckTroniiKz@users.noreply.github.com>
2024-01-12 10:47:32 +0100

    add needed methods, and test method for text extractions

---
+++ Aufgabe 1/main.py
@@ -0,0 +1,22 @@
+"""
+    a)  Programmieren Sie eine Python-Funktion für das Ranking von Art Stories im 
+        Word2Vec Embedding, die einer Anfrage durch einen Besucher Ihrer Kunst-Website 
+        am ähnlichsten sind.
+    b)  Beurteilen Sie den Einfluss der Erweiterung auf das Doc2Vec 
+        Embedding auf die Ergebnisqualität.
+    c)  Programmieren Sie einen Empfehlungsdienst für Art Stories durch Clustering mit Top2Vec.
+"""
+
+def read_document_text(path):
+    document_lines = []
+    with open(path, "r") as document:
+        for line in document:
+            document_lines.append(line)
+    document.close()
+
+
+def rank_art_stories_python_function(user_query_string):
+    pass
+
+def recommend_art_stories_python_function(identifier_of_visited_art_stories_list):
+    pass
