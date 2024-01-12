9e9ee26ee2590aa5c8d5ee5977dd55c160739ce6
Can Cetin <73250162+EleckTroniiKz@users.noreply.github.com>
2024-01-12 12:23:22 +0100

    add file from rapp, work on read into dataframe

--- Aufgabe 1/main.py
+++ Aufgabe 1/main.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import re
+
 """
     a)  Programmieren Sie eine Python-Funktion für das Ranking von Art Stories im 
         Word2Vec Embedding, die einer Anfrage durch einen Besucher Ihrer Kunst-Website 
@@ -9,17 +12,28 @@
 
 corpus = []
 
-def read_document_text(path):
-    document_lines = []
-    with open(path, "r") as document:
-        for line in document:
-            document_lines.append(line)
-            words = line.split(" ")
-            for word in words:
-                if word not in corpus:
-                    corpus.append(word)
-    document.close()
+def clean_text(text_to_clean):
+    """ This method will receive a string, and returns the same string, cleaned of any syntactical symbols like commas, parentheses, semicoli, ...
+    """
+    # first remove line breaks in the text
+    string_without_linebreaks = text_to_clean.replace('\n', ' ')
+    # then remove NON alphabetical and NON numerical values
+    cleaned_string = re.sub(r'[a-zA-Z0-9]', ' ', string_without_linebreaks)
+    return cleaned_string
 
+def read_document_text(path="./art_stories_examples.csv"):
+    """ This method reads the data of a file which is a CSV, and has the content Semikolon(;) separated. 
+        The columns are:  ContentType - Title - ShortDescriptionText - Text
+    """
+    # CAN MACHT DIESE METHODE GRAD BIDDE NIT ANFASSEN :D
+    # read file into the dataframe
+    dataframe = pd.read_csv(path, sep=";")
+    for row in dataframe.iterrows():
+        print(row)
+
+    pass
+
+read_document_text()
 
 def rank_art_stories_python_function(user_query_string):
     pass
