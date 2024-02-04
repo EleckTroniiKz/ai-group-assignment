59fce865788906853d6e566da3b981b4202ebdd8
Can Cetin <73250162+EleckTroniiKz@users.noreply.github.com>
2024-02-04 20:32:40 +0100

    Task d .... Prototype

---
+++ Aufgabe 3/sparql_writer.py
@@ -0,0 +1,24 @@
+
+
+def natural_language_to_sparql(natural_language_query_string):
+    """
+    Convert a natural language query to a SPARQL query.
+    :param natural_language_query: The natural language query.
+    :return: The SPARQL query.
+    """
+
+    """ PROBLEM:
+        The problem is, that in the code, a package which is used tries to reference sklearn.ensemble.forest. 
+        But that is deprecated. So the code needs to be updated to use the new package.
+        But I can not update the code of the package. 
+        So I won't be able to test out, which will work and which won't. But I still want to explain the general
+        idea what we would go for to solve the task.
+    """
+
+    #IDEA
+
+    # After Training the Model, the test Files are the ones, which allow us to test. 
+    # There 
+
+
+    return natural_language_query_string
