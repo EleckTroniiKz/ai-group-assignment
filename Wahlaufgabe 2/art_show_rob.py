ea143b829067498fbe0c7ac2adca91413ba150da
Can Cetin <73250162+EleckTroniiKz@users.noreply.github.com>
2024-02-19 11:08:30 +0100

    add state_table

--- Wahlaufgabe 2/art_show_rob.py
+++ Wahlaufgabe 2/art_show_rob.py
@@ -18,7 +18,7 @@ def read_xslx_into_array(file_path="C:\\Users\\Can\\Documents\\Programming\\ai-p
     result = [inner[2:] for inner in result[2:]]
     return result
 
-def create_q_table():
+def create_q_table_and_state_table():
     nested_list = read_xslx_into_array()
     dict = {}
 
@@ -28,36 +28,49 @@ def create_q_table():
             if value != -1:
                 dict[i][j] =  value
     q_table = {}
+    state_table = {}
 
     for i in dict.keys():
         q_table[i] = {}
+        state_table[i] = {}
         for j in dict[i].keys():
             if i == 12:
                 q_table[i]["R"] = dict[i][j]
+                state_table[i]["R"] = 7
                 continue
             elif i == 20:
                 q_table[i]["L"] = dict[i][j] 
+                state_table[i]["L"] = 11
             elif i == 7:
                 q_table[i]["L"] = dict[i][12]
                 q_table[i]["R"] = dict[i][13]
+                state_table[i]["L"] = 12
+                state_table[i]["R"] = 13
             elif i == 11:
                 q_table[i]["L"] = dict[i][19]
                 q_table[i]["R"] = dict[i][20]
+                state_table[i]["L"] = 19
+                state_table[i]["R"] = 20
             if j-1 == i:
                 q_table[i]["R"] = dict[i][j]
+                state_table[i]["R"] = j
             elif j+1 == i:
                 q_table[i]["L"] = dict[i][j]
+                state_table[i]["L"] = j
             elif j+1 < i:
                 q_table[i]["U"] = dict[i][j]
+                state_table[i]["U"] = j
             elif j-1 > i:
                 q_table[i]["D"] = dict[i][j] 
+                state_table[i]["D"] = j
 
-    return q_table          
+    return q_table, state_table          
 
-q_table = create_q_table()
+q_table, state_table = create_q_table_and_state_table()
 
-# to use q_table, you have to call q_table[state]][action] for example. The Actions are defined in the Action Enum
+
 
 """
-    e.g. q_table[5][Action.UP] -> 5 is the state and Action.UP is the action
+    q_table will return the reward that will be granted, when being at state x, and doing action y -> q_table[x][y]. All available actions can be received by calling q_table[x].keys()
+    state_table will return the state that will be reached, when being at state x, and doing action y -> state_table[x][y]. All available actions can be received by calling state_table[x].keys(). 
 """
