03318ccc66a9b7a6da074af8b9b4fc7d4b2c36d2
Can Cetin <73250162+EleckTroniiKz@users.noreply.github.com>
2024-02-20 18:09:10 +0100

    Comment 6.

--- Wahlaufgabe 2/art_show_rob.py
+++ Wahlaufgabe 2/art_show_rob.py
@@ -1,6 +1,9 @@
 import openpyxl
 from enum import Enum
-
+import time
+from random import randint
+import random
+import pandas as pd
 import pygame
 
 class Action(Enum):
@@ -124,6 +127,7 @@ def read_xslx_into_array(file_path="C:\\Users\\Can\\Documents\\Programming\\ai-p
 
 def create_q_table_and_state_table(reward_matrix):
     
+ 
     dict = {}
 
     for i, inner_list in enumerate(reward_matrix):
@@ -143,7 +147,7 @@ def create_q_table_and_state_table(reward_matrix):
                 state_table[i]["R"] = 7
                 continue
             elif i == 20:
-                q_table[i]["L"] = dict[i][j] 
+                q_table[i]["L"] = dict[i][j]
                 state_table[i]["L"] = 11
                 continue
             elif i == 7:
@@ -166,18 +170,6 @@ def create_q_table_and_state_table(reward_matrix):
                 state_table[i]["U"] = 6
                 state_table[i]["D"] = 30
                 continue
-            elif i == 13:
-                q_table[i]["L"] = dict[i][7]
-                q_table[i]["R"] = dict[i][14]
-                state_table[i]["L"] = 7
-                state_table[i]["R"] = 14
-                continue
-            elif i == 19:
-                q_table[i]["L"] = dict[i][18]
-                q_table[i]["R"] = dict[i][11]
-                state_table[i]["L"] = 18
-                state_table[i]["R"] = 11
-                continue
             if j-1 == i:
                 q_table[i]["R"] = dict[i][j]
                 state_table[i]["R"] = j
@@ -188,31 +180,206 @@ def create_q_table_and_state_table(reward_matrix):
                 q_table[i]["U"] = dict[i][j]
                 state_table[i]["U"] = j
             elif j-1 > i:
-                q_table[i]["D"] = dict[i][j] 
+                q_table[i]["D"] = dict[i][j]
                 state_table[i]["D"] = j
 
-    return q_table, state_table          
+    return q_table, state_table     
 
-def remove_reward_from_matrix(matrix, state):
-    """
-    might work already. NOts ure
-    """
-    for i in range(len(matrix)):
-        matrix[i][state] = 0
-    return matrix
 
 reward_matrix = read_xslx_into_array()
 q_table, state_table = create_q_table_and_state_table(reward_matrix)
-
-plot_movements([0, 5, 8, 12, 20, 1])
-
-"ich berechne den nächsten move"
-# remove_reward_from_matrix(reward_matrix, state) # state is the state that the robot is currently in
-# q_table, state_table = create_q_table_and_state_table(reward_matrix)
+new_q_table, new_state_table = q_table, state_table
+new_reward_matrix = reward_matrix
 
 
 
 """
     q_table will return the reward that will be granted, when being at state x, and doing action y -> q_table[x][y]. All available actions can be received by calling q_table[x].keys()
     state_table will return the state that will be reached, when being at state x, and doing action y -> state_table[x][y]. All available actions can be received by calling state_table[x].keys(). 
+"""
+
+def create_random_action():
+    """
+    :return: Random possible action for robot to execute.
+    """
+    possible_action = {1: 'L', 2: 'R', 3: 'U', 4: 'D'}
+    rand_number = randint(1, 4)
+    return possible_action[rand_number]
+
+def is_action_valid(current_state_number, action):
+    """
+    Validates the next move of the roboter.
+    :param current_state_number: Integer from [0 , ... , 30]
+    :param action: String
+    :return: boolean (Is action valid -> true, is action invalid -> false
+    """
+    for key in state_table[current_state_number]:
+        if key == action:
+            return True
+        else:
+            continue
+    return False
+
+
+def create_new_reward_matrix(reward_matrix, action):
+    """
+    :param reward_matrix: The given xlsx reward matrix
+    :param action: The action the robot executes
+    :return: new_reward_matrix -> Updated xlsx with the new rewards
+    """
+    column_index = action
+    new_reward_matrix = reward_matrix
+    for row in new_reward_matrix:
+        if row[column_index] != -1:
+            row[column_index] = -1
+    return new_reward_matrix
+
+
+def get_best_action_from_q_table(current_state_number):
+    """
+    :param current_state_number: The current state of the robot.
+    :return: the maximum reward possible for the robot (greedy method).
+    """
+    best_reward = 0
+    best_action = 0
+    for key in new_q_table[current_state_number]:
+        value = new_q_table[current_state_number][key]
+        if value >= best_reward:
+            best_reward = value
+            best_action = key
+
+    if best_reward == 0:
+        best_action = create_random_action()
+    #print(best_action)
+    #print(best_reward)
+    return best_action, best_reward
+
+
+def create_random_service_task():
+    """
+    This is a method for phase 3. Creates a random "service task" and updates the q-table regarding the reward.
+    :return: the updated q-table with new "services"
+    """
+    global new_reward_matrix, new_q_table
+    rand_number_state = randint(0, 30)
+    rand_number_reward = random.choice([1, 5, 10])
+    possible_actions = []
+    for value in state_table[rand_number_state].values():
+        possible_actions.append(value)
+    for row_index in possible_actions:
+        if new_reward_matrix[row_index][rand_number_state] == -1:
+            new_reward_matrix[row_index][rand_number_state] = rand_number_reward
+    create_q_table_and_state_table(new_reward_matrix)
+
+
+def optimal_strategy_function(current_timestep, current_phase_number, current_state_number, current_reward_matrix_dataframe):
+    """
+    Implements the optimal strategy function for three given phases.
+    Phase 1: No service requests.
+    Phase 2: Available service requests given via Matrix.
+    Phase 3: Service requests are always available and random new requests are handled.
+
+    :param current_timestep: Integer increment
+    :param current_phase_number: [1,2,3]
+    :param current_state_number: [0, ... , 30]
+    :param current_reward_matrix_dataframe: 31x31 Matrix
+    :return: the optimal strategy path as an array [0....30] the initial steps
+    """
+
+    list_of_rewards = []
+    new_state_number = current_state_number
+    global new_reward_matrix, new_q_table, new_state_table
+    new_reward_matrix = current_reward_matrix_dataframe.to_numpy()
+    path = [current_state_number]
+    print("PATH", path)
+    exit_state = 20
+
+    # Phase 1
+    if current_phase_number == 1:  # Walk until EXIT or time expires and than print out the explored (probably bad path)
+        print("PHASE 1 EXECUTED")
+        starting_time = current_timestep
+        while True:
+            current_time = time.time()
+            elapsed_time = current_time - starting_time
+            if elapsed_time >= 60:  # Try to execute phase one for a minute...
+                break
+            random_action = create_random_action()
+            is_valid = is_action_valid(new_state_number, random_action)
+            if is_valid:
+                reward = q_table[new_state_number][random_action]
+                list_of_rewards.append(reward)
+                new_state_number = state_table[new_state_number][random_action]
+                path.append(new_state_number)
+
+            if new_state_number == exit_state:
+                print('Total Path:', path)
+                print('Reward List:', list_of_rewards)
+                return path
+
+    # Phase 2
+    elif current_phase_number == 2:
+        print("PHASE 2 EXECUTED")
+        while True:
+            best_action, best_reward = get_best_action_from_q_table(new_state_number)
+            is_valid = is_action_valid(new_state_number, best_action)
+            if is_valid:
+                list_of_rewards.append(best_reward)
+                new_state_number = state_table[new_state_number][best_action]
+                path.append(new_state_number)
+                new_reward_matrix = create_new_reward_matrix(new_reward_matrix, new_state_number)
+                new_q_table, new_state_table = create_q_table_and_state_table(new_reward_matrix)
+                #print(pd.DataFrame(new_reward_matrix))
+                #print(new_q_table)
+            print('Total Path:', path)
+            print('Reward List:', list_of_rewards)
+            exit_condition = -1
+            # Iteriere über alle Zeilen
+            for i in range(len(new_reward_matrix)):
+                # Iteriere über alle Spalten
+                for j in range(len(new_reward_matrix[i])):
+                    if new_reward_matrix[i][j] != -1:
+                        exit_condition = new_reward_matrix[i][j]
+
+            if exit_condition == -1:
+                break
+        return path
+
+    # Phase 3
+    else:
+        print("PHASE 3 EXECUTED")
+        starting_time = current_timestep
+        while True:
+            current_time = time.time()
+            elapsed_time = current_time - starting_time
+            if elapsed_time >= 6:  # Try to execute phase three for six seconds...
+                break
+            create_random_service_task()
+            best_action, best_reward = get_best_action_from_q_table(new_state_number)
+            is_valid = is_action_valid(new_state_number, best_action)
+            if is_valid:
+                list_of_rewards.append(best_reward)
+                new_state_number = state_table[new_state_number][best_action]
+                path.append(new_state_number)
+                new_reward_matrix = create_new_reward_matrix(new_reward_matrix, new_state_number)
+                new_q_table, new_state_table = create_q_table_and_state_table(new_reward_matrix)
+                #print(pd.DataFrame(new_reward_matrix))
+                #print(new_q_table)
+            print('Total Path:', path)
+            print('Reward List:', list_of_rewards)
+            return path
+
+
+start_time = time.time()
+for index in range(1, 4):
+    path = optimal_strategy_function(start_time, 3, 3, pd.DataFrame(reward_matrix))
+
+
+"""
+    Beurteilen Sie, inwiefern der Einsatz von Dataset Aggregation zur Effizienzsteigerung Ihres Roboters bei einer realen Kunstmesse beitragen kann.
+
+    Dataset Aggregation allows us to combine Data from different sources to train the model. In the case how we learned it in class, it is by "demonstrating" to the robot. And then iteratively letting the robot try, and find out how to maximize his reward during the process.
+    DAgger can help to reduce overfitting, because we have more Data available. In this case the people's input. And also in the context of the art gallery, it can help to make the robot more dynamic and flexible. It can learn from the people's input and adapt to the environment, even when the layout changes. 
+    
+    So all in all, can the robot learn from the people's input and its already existing knowledge, and be more efficient with those, and prevent overfitting. 
+
 """
