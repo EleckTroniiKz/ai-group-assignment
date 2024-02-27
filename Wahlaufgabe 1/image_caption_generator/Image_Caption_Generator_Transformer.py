db3cab848775b7c2793550204174ae9f1dfba1a8
Marvin <marvin@oberts.de>
2024-02-27 16:11:45 +0100

    use new model

--- Wahlaufgabe 1/image_caption_generator/Image_Caption_Generator_Transformer.py
+++ Wahlaufgabe 1/image_caption_generator/Image_Caption_Generator_Transformer.py
@@ -1,33 +1,27 @@
-#Image-caption-generator
-#This model is trained on Flickr8k dataset to generate captions given an image.
-#https://huggingface.co/bipin/image-caption-generator
-
-#1. Load the pre-trained model from the model hub
+# 1. Load the pre-trained model from the model hub
 from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
 import torch
 from PIL import Image
 
-model_name = "bipin/image-caption-generator"
+model_name = "nlpconnect/vit-gpt2-image-captioning"
 
 # load model
 model = VisionEncoderDecoderModel.from_pretrained(model_name)
 feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-
-#2. Load the image for which the caption is to be generated
-#img_name = "flickr_data.jpg"
-img_name = "37.jpg"
+# 2. Load the image for which the caption is to be generated
+# img_name = "flickr_data.jpg"
+print("Model loaded")
+img_name = "35.png"
 img_path = "image_data/FiftyArtImages/" + img_name
-#img = Image.open(img_name)
+# img = Image.open(img_name)
 img = Image.open(img_path)
 if img.mode != 'RGB':
     img = img.convert(mode="RGB")
-
-
 
 pixel_values = feature_extractor(images=[img], return_tensors="pt").pixel_values
 pixel_values = pixel_values.to(device)
@@ -36,6 +30,7 @@ max_length = 128
 num_beams = 4
 
 # get model prediction
+print("Pixel preprocessed")
 output_ids = model.generate(pixel_values, num_beams=num_beams, max_length=max_length)
 
 # decode the generated prediction
